2fd7f2c765b4cbc41597d471e3f1dfdc02931412
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-08 16:17:51 -0600

    messing around with array mutability

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,37 +1,4 @@
-const isInteresting = (num, phrases) => {
-	if (true) {
-		let coolNum = false;
-	}
-	if (num == 98 || num == 99 || num == 1232) {
-		return 1;
-	} else if (num < 100) {
-		return 0;
-	}
-	for (let i = 2; i > -1; i--) {
-		let numArr = num.toString(10).split("").map(Number);
-		for (let j = 0; j < numArr.length - 1; j++) {
-			if (
-				numArr[j] + 1 == numArr[j + 1] ||
-				(numArr[j] == numArr[numArr.length - (j + 1)] &&
-					numArr[j + 1] == numArr[numArr.length - (j + 2)]) ||
-				numArr[j] - 1 == numArr[j + 1]
-			) {
-				coolNum = true;
-			} else if (numArr[j] == 9 && numArr[j + 1] == 0) {
-				coolNum == true;
-			} else {
-				coolNum = false;
-				j += numArr.length;
-			}
-		}
-		if (
-			coolNum ||
-			phrases.includes(num) ||
-			numArr.filter((num, i) => i != 0).join("") == 0
-		) {
-			return i == 0 ? 1 : i;
-		}
-		num += 1;
-	}
-	return 0;
-};
+let numArr = [5,4,3,2,1]
+let x = numArr.map(num => num)
+        x.sort()
+        console.log(x, numArr) // => [1,2,3,4,5], [1,2,3,4,5]
