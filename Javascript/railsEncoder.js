2781b657eeda6901a4498cd12481e6fa386dcf32
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-10 13:15:11 -0600

    decode works if num of rails is 3

--- Javascript/railsEncoder.js
+++ Javascript/railsEncoder.js
@@ -1,8 +1,9 @@
 //https://www.codewars.com/kata/58c5577d61aefcf3ff000081/train/javascript
 
-function encodeRailFenceCipher(string, numberRails) {
+function encodeRailFenceCipher(string, numberRails, lengths) {
     let letterArray = string.split('')
-    let res = [[],[],[]]
+    let res = []
+    while(res.length != numberRails){res.push([])}
     let count = 1;
     let countingUp = true
     for(let i = 0; i < letterArray.length; i++){
@@ -15,15 +16,37 @@ function encodeRailFenceCipher(string, numberRails) {
         }
         countingUp ? count += 1 : count = count - 1
     }
-    return res.map(arr => arr.join('')).join('')
+    // console.log(res)
+    return lengths ? res.map(arr => arr.length) : res.map(arr => arr.join('')).join('')
+    //res.map(arr => arr.join('')).join('')
   }
 
-  console.log(encodeRailFenceCipher("Hello, World!", 3),)
-    //"Hoo!el,Wrdl l"
-   //  Hoo!el,Wrdl l
-  //"WECRLTEERDSOEEFEAOCAIVDEN"
- //  WECRLTEERDSOEEFEAOCAIVDEN
+  console.log(encodeRailFenceCipher("WEAREDISCOVEREDFLEEATONCE", 3)) // WECRLTEERDSOEEFEAOCAIVDEN
 
-//   W       E       C       R       L       T       E
-//    E   R   D   S   O   E   E   F   E   A   O   C  
-//     A       I       V       D       E       N    
+  function decodeRailFenceCipher(string, numberRails) {
+    let letterArray = string.split('')
+    let res = []
+    let decodedLetters = []
+    while(res.length != numberRails){res.push([])}
+    let lengths = encodeRailFenceCipher(string, numberRails, true)
+    let count = 0
+    // console.log(res[0].length)
+    for(let i = 0; i < letterArray.length; i ++){
+        res[count].length < lengths[count] ? res[count].push(letterArray[i]) 
+        : (count += 1, res[count].push(letterArray[i]))
+        }
+    for(let i = 0; i < letterArray.length; i ++){
+        count = 0
+        while(count <= numberRails - 1){
+            decodedLetters.push([res[count][i]])
+            count += 1  
+        }
+        count = count - 2
+        decodedLetters.push(res[count][i + 1])
+        res[count].splice([i + 1],1)
+    }
+    return decodedLetters.join('')
+  }
+
+
+  console.log(decodeRailFenceCipher('WIREEEDSEEEACAECVDLTNROFO', 4)) // => "WEAREDISCOVEREDFLEEATONCE"
