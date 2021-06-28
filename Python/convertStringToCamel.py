db5481dd008690cb0307516646621714379b251f
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-28 15:57:36 -0600

    convertToCamel done

--- Python/convertStringToCamel.py
+++ Python/convertStringToCamel.py
@@ -1,13 +1,16 @@
 def to_camel_case(text):
-    wordList = text.split('_')
+    wordList = '-'.join(text.split('_')).split('-')
     resList = []
     for i in range(len(wordList)):
         resList.append(wordList[i]) if i == 0 else resList.append(wordList[i].capitalize())
-    res = ''.join(resList)    
-    return res
+    return ''.join(resList)    
 
 
 text1 = 'the_stealth_warrior'
-text2= ''
+text2= 'ABC'
+text3 = 'the-stealth-warrior'
 print(to_camel_case(text1))
 print(to_camel_case(text2))
+print(to_camel_case(text3))
+
+""" first python kata!
