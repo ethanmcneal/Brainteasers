a959bfa76a1aaa39bb1ad419ce27c9ce0d177d35
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-05 12:44:11 -0600

    optimized to end loop when target value is found

--- Javascript/twoSum.js
+++ Javascript/twoSum.js
@@ -2,15 +2,14 @@
 // target
 
 const twoSum = (nums, target) => {
-    let res
     for(let i = 0; i < nums.length; i++){
         for(let j = 0; j < nums.length; j++ ){
             if(j !== i && nums[i] + nums[j] == target){
-                res = [j, i]
+                return [i, j]
             }   
         }
     }
-    return res
+    return -1 //returns -1 if no nums add up to the target
 }
 
 console.log(twoSum([1,2,3,4,5], 3))
