140cb123587c4d49035ae3328acb5ef918316b5c
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-12 14:52:32 -0600

    prettier

--- Javascript/findTheUnknownDigit.js
+++ Javascript/findTheUnknownDigit.js
@@ -1,100 +1,85 @@
 //https://www.codewars.com/kata/546d15cebed2e10334000ed9/train/javascript
 
-
 function solveExpression(exp) {
-  let arr = exp.split("=")[0].toString().split("");
-  let expressions = ["-", "+", "*"];
-  let firstNum = "";
-  let secondNum = "";
-  let expression = "";
-  let answer = exp.split("=").pop();
-  let currentPos = 1;
-  let res = -1;
-  for (let i = 0; i < arr.length; i++) {
-    // arr[i] == "?" ? (arr[i] = "g") : "";
-    if (!expressions.includes(arr[i])) {
-      currentPos == 1
-        ? (firstNum += arr[i])
-        : currentPos == 2
-        ? (secondNum += arr[i])
-        : "";
-    } else if (arr[i] == "-") {
-      firstNum == ""
-        ? (firstNum += arr[i])
-        : expression !== ""
-        ? (secondNum += arr[i])
-        : ((expression += arr[i]), (currentPos += 1));
-    } else {
-      expression += arr[i];
-      currentPos += 1;
-    }
-  }
-  let j = 0;
-  if (answer.replace(/\?/g, `${j}`).split("-").join('').split('')[0] == "0" && answer.length > 1) {
-    j += 1;
-  }
-  for (j; j < 10; j++) {  
-    while(
-      firstNum.split("").includes(`${j}`) ||
-      secondNum.split("").includes(`${j}`) ||
-      answer.split("").includes(`${j}`)
-    ) {
-      j += 1;
-    }
+	let arr = exp.split("=")[0].toString().split("");
+	let expressions = ["-", "+", "*"];
+	let firstNum = "";
+	let secondNum = "";
+	let expression = "";
+	let answer = exp.split("=").pop();
+	let currentPos = 1;
+	let res = -1;
+	for (let i = 0; i < arr.length; i++) {
+		// arr[i] == "?" ? (arr[i] = "g") : "";
+		if (!expressions.includes(arr[i])) {
+			currentPos == 1
+				? (firstNum += arr[i])
+				: currentPos == 2
+				? (secondNum += arr[i])
+				: "";
+		} else if (arr[i] == "-") {
+			firstNum == ""
+				? (firstNum += arr[i])
+				: expression !== ""
+				? (secondNum += arr[i])
+				: ((expression += arr[i]), (currentPos += 1));
+		} else {
+			expression += arr[i];
+			currentPos += 1;
+		}
+	}
+	let j = 0;
+	if (
+		answer.replace(/\?/g, `${j}`).split("-").join("").split("")[0] == "0" &&
+		answer.length > 1
+	) {
+		j += 1;
+	}
+	for (j; j < 10; j++) {
+		while (
+			firstNum.split("").includes(`${j}`) ||
+			secondNum.split("").includes(`${j}`) ||
+			answer.split("").includes(`${j}`)
+		) {
+			j += 1;
+		}
 
-    let x = firstNum.replace(/\?/g, `${j}`);
-    let y = secondNum.replace(/\?/g, `${j}`);
-    let z = answer.replace(/\?/g, `${j}`);
+		let x = firstNum.replace(/\?/g, `${j}`);
+		let y = secondNum.replace(/\?/g, `${j}`);
+		let z = answer.replace(/\?/g, `${j}`);
 
-    if (expression == "+") {
-      if (parseInt(x) + parseInt(y) == parseInt(z)) {
-        res = j;
-        j += 10;
-      }
-    }
-    if (expression == "-") {
-      if (parseInt(x) - parseInt(y) == parseInt(z)) {
-        res = j;
-        j += 10;
-      }
-    }
+		if (expression == "+") {
+			if (parseInt(x) + parseInt(y) == parseInt(z)) {
+				res = j;
+				j += 10;
+			}
+		}
+		if (expression == "-") {
+			if (parseInt(x) - parseInt(y) == parseInt(z)) {
+				res = j;
+				j += 10;
+			}
+		}
 
-    if (expression == "*") {
-      if (parseInt(x) * parseInt(y) == parseInt(z)) {
-        res = j;
-        j += 10;
-      }
-    }
-  }
-  return res;
+		if (expression == "*") {
+			if (parseInt(x) * parseInt(y) == parseInt(z)) {
+				res = j;
+				j += 10;
+			}
+		}
+	}
+	return res;
 }
 
-
-// .replace('--', '+') way, top solution on codewars much more elegant than mine
-
+//old regex way, wont solve all test cases
 function solveExpressionRegex(exp) {
-    exp = exp.replace('=','==').replace('--','+');
-    for(var i = 0; i < 10; i++){
-      if(eval(exp.replace(/\?/g,i)) && !exp.includes(i)){
-          if(!/^00+$/.test(exp.replace(/\?/g,i).split('==')[1]))  return i;
-      }
-    }
-    return -1;
-  }
+	exp = exp.replace("=", "==").replace("--", "+");
+	for (var i = 0; i < 10; i++) {
+		if (eval(exp.replace(/\?/g, i)) && !exp.includes(i)) {
+			if (!/^00+$/.test(exp.replace(/\?/g, i).split("==")[1])) return i;
+		}
+	}
+	return -1;
+}
 
-
-  module.exports = solveExpression
-// describe('Fixed tests', function() {
-//     it('Example tests', function() {
-//       var data = [
-//         ['1+1=?', 2],
-//         ['123*45?=5?088', 6],
-//         ['-5?*-1=5?', 0],
-//         ['19--45=5?', -1],
-//         ['??*??=302?', 5],
-//         ['?*11=??', 2],
-//         ['??*1=??', 2],
-//         ['??+??=??', -1]];
-//       for(let [exp, expected] of data) Test.assertEquals(solveExpression(exp), expected);
-//     });
-//   });
+module.exports = solveExpression;
--- Javascript/tests/findTheUnknownDigit.test.js
+++ Javascript/tests/findTheUnknownDigit.test.js
@@ -1,28 +1,26 @@
+const solveExpression = require("../findTheUnknownDigit");
 
+test("addition", () => {
+	expect(solveExpression("1+1=?")).toEqual(2);
+	expect(solveExpression("1+?=5")).toEqual(4);
+});
 
-const solveExpression = require('../findTheUnknownDigit')
+test("multiplication", () => {
+	expect(solveExpression("123*45?=5?088")).toEqual(6);
+	expect(solveExpression("??*??=302?")).toEqual(5);
+});
 
-test('addition', () => {
-    expect(solveExpression('1+1=?')).toEqual(2)
-    expect(solveExpression('1+?=5')).toEqual(4)
-})
+test("negatives", () => {
+	expect(solveExpression("-5?*-1=5?")).toEqual(0);
+	expect(solveExpression("-5--?=-1")).toEqual(4);
+});
 
-test('multiplication', () => {
-    expect(solveExpression('123*45?=5?088')).toEqual(6)
-    expect(solveExpression('??*??=302?')).toEqual(5)
-})
+test("unsolvable => -1", () => {
+	expect(solveExpression("??+??=??")).toEqual(-1);
+	expect(solveExpression("19--45=5?")).toEqual(-1);
+});
 
-test('negatives', () => {
-    expect(solveExpression('-5?*-1=5?')).toEqual(0)
-    expect(solveExpression('-5--?=-1')).toEqual(4)
-})
-
-test('unsolvable => -1', () => {
-    expect(solveExpression('??+??=??')).toEqual(-1)
-    expect(solveExpression('19--45=5?')).toEqual(-1)
-})
-
-test('? cannot be number already defined in expression', () => {
-    expect(solveExpression('?*11=??')).toEqual(2)
-    expect(solveExpression('??*1=??')).toEqual(2)
-})
+test("? cannot be number already defined in expression", () => {
+	expect(solveExpression("?*11=??")).toEqual(2);
+	expect(solveExpression("??*1=??")).toEqual(2);
+});
