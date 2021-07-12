4a2afadd580a461f085ad88b32ca5848c6e0435a
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-12 16:16:36 -0600

    range extraction

--- Javascript/tests/rangeExtraction.test.js
+++ Javascript/tests/rangeExtraction.test.js
@@ -1,4 +1,11 @@
-const { test } = require('@jest/globals')
-const solution = require('../rangeExtraction')
+const { test, expect } = require("@jest/globals");
+const solution = require("../rangeExtraction");
 
-test('')
+test("if an integers count up from each-other +1, reformat array to combine them", () => {
+	expect(
+		solution([
+			-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19,
+			20,
+		]),
+	).toEqual("-6,-3-1,3-5,7-11,14,15,17-20");
+});
