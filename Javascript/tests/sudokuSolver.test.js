9bcbfd10408d8287c42b431d5597ad980971f000
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-14 12:21:32 -0600

    finish tests for sudoku solver

--- Javascript/tests/sudokuSolver.test.js
+++ Javascript/tests/sudokuSolver.test.js
@@ -1,6 +1,5 @@
-const { test, expect } = require('@jest/globals')
-const sudoku = require('../sudokuSolver')
-
+const { test, expect } = require("@jest/globals");
+const sudoku = require("../sudokuSolver");
 
 function validSolution(board) {
 	let valid = true;
@@ -11,24 +10,24 @@ function validSolution(board) {
 		} else {
 			return true;
 		} //takes a column, row, or cube, abd makes a set, if the set.length === 9 is true or does not include a 0, returns true
-	}
+	};
+    //row testing
 	for (i = 0; i < board.length; i++) {
 		if (!valid) break;
-		let row = []
+		let row = [];
 		for (j = 0; j < board.length; j++) {
-			row.push(board[i][j])
+			row.push(board[i][j]);
 		}
-		// console.log(row)
-		valid = testValiditiy(row)
+		valid = testValiditiy(row);
 	}
 
 	//column testing
 	for (i = 0; i < board.length; i++) {
-		let col = []
+		let col = [];
 		for (j = 0; j < board.length; j++) {
-			col.push(board[j][i])
+			col.push(board[j][i]);
 		}
-		valid = testValiditiy(col)
+		valid = testValiditiy(col);
 		if (!valid) break;
 	}
 
@@ -36,48 +35,47 @@ function validSolution(board) {
 	let row = 0;
 	let column = 0;
 	while (row < 9 && column < 9) {
-		let cube = []
+		let cube = [];
 		for (i = row; i < row + 3; i++) {
 			for (j = column; j < column + 3; j++) {
-				cube.push(board[i][j])
+				cube.push(board[i][j]);
 			}
 		}
-		row += 3
-		valid = testValiditiy(cube)
+		row += 3;
+		valid = testValiditiy(cube);
 		if (!valid) break;
 		if (row == 9 && column != 9) {
-			column += 3
+			column += 3;
 			row = 0;
 		}
 	}
-	return valid
+	return valid;
 }
 let puzzleOne = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-  ];
+	[5, 3, 0, 0, 7, 0, 0, 0, 0],
+	[6, 0, 0, 1, 9, 5, 0, 0, 0],
+	[0, 9, 8, 0, 0, 0, 0, 6, 0],
+	[8, 0, 0, 0, 6, 0, 0, 0, 3],
+	[4, 0, 0, 8, 0, 3, 0, 0, 1],
+	[7, 0, 0, 0, 2, 0, 0, 0, 6],
+	[0, 6, 0, 0, 0, 0, 2, 8, 0],
+	[0, 0, 0, 4, 1, 9, 0, 0, 5],
+	[0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
 
-  let puzzleTwo = [ 
-  [3, 0, 6, 5, 0, 8, 4, 0, 0], 
-  [5, 2, 0, 0, 0, 0, 0, 0, 0], 
-  [0, 8, 7, 0, 0, 0, 0, 3, 1], 
-  [0, 0, 3, 0, 1, 0, 0, 8, 0], 
-  [9, 0, 0, 8, 6, 3, 0, 0, 5], 
-  [0, 5, 0, 0, 9, 0, 6, 0, 0], 
-  [1, 3, 0, 0, 0, 0, 2, 5, 0], 
-  [0, 0, 0, 0, 0, 0, 0, 7, 4], 
-  [0, 0, 5, 2, 0, 6, 3, 0, 0] ]
+let puzzleTwo = [
+	[3, 0, 6, 5, 0, 8, 4, 0, 0],
+	[5, 2, 0, 0, 0, 0, 0, 0, 0],
+	[0, 8, 7, 0, 0, 0, 0, 3, 1],
+	[0, 0, 3, 0, 1, 0, 0, 8, 0],
+	[9, 0, 0, 8, 6, 3, 0, 0, 5],
+	[0, 5, 0, 0, 9, 0, 6, 0, 0],
+	[1, 3, 0, 0, 0, 0, 2, 5, 0],
+	[0, 0, 0, 0, 0, 0, 0, 7, 4],
+	[0, 0, 5, 2, 0, 6, 3, 0, 0],
+];
 
-
-test('solves the sudoku puzzle', () => {
-    expect(validSolution(sudoku(puzzleOne))).toBeTruthy()
-    expect(validSolution(sudoku(puzzleTwo))).toBeTruthy()
-
-})
+test("tests if solution is valid", () => {
+	expect(validSolution(sudoku(puzzleOne))).toBeTruthy();
+	expect(validSolution(sudoku(puzzleTwo))).toBeTruthy();
+});
