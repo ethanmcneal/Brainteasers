8ff460e9756eb28b92366b404297f3b739c2728b
ethan.mcneal <ethan.mcneal@gmail.com>
2021-07-14 12:38:09 -0600

    basic test setup

--- Javascript/tests/twoSum.test.js
+++ Javascript/tests/twoSum.test.js
@@ -1,1 +1,6 @@
+const { test, expect } = require('@jest/globals')
 const twoSum = require('../twoSum')
+
+test('takes an array and target value, returns the indecies of the two values that add up to target', () => {
+    expect(twoSum([1,2,3,4,5], 9)).toStrictEqual([3,4])
+})
--- Javascript/twoSum.js
+++ Javascript/twoSum.js
@@ -12,8 +12,6 @@ const twoSum = (nums, target) => {
     return -1 //returns -1 if no nums add up to the target
 }
 
-console.log(twoSum([1,2,3,4,5], 3))
-
 // function that takes an array of numbers, and a target value,
 // return the indexes of the two nums that add up to the target value
 
